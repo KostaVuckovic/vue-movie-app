1a9e4bcf8212ba296815b561ce89a5bddc923f9c
KostaVuckovic <vuckovicr1@gmail.com>
2020-08-03 16:16:48 +0200

    postcss whitelist added

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     './src/**/*.jsx',
     // etc.
   ],
-  whitelist: [/content-wrapper/, /search-input/, /vue-loading/], 
+  whitelist: [/content-wrapper/, /search-input/, /vue-loading-overlay/], 
   whitelistPatterns: [/enter$/, /enter-to$/, /leave$/, /leave-to$/, /leave-active$/, /enter-active$/, /move$/,],
 
   // This is the function used to extract class names from your templates
